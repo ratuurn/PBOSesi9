12c203dbe92f5783d027ac669a412866fdcb9823
ratuurn <114390295+ratuurn@users.noreply.github.com>
2022-12-23 17:53:43 +0700

    Create Hero.java

---
+++ PBONo2/Game/Hero.java
@@ -0,0 +1,9 @@
+package PBONo2.Game;
+
+public abstract class Hero {
+    abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoHero();
+}
